e89df9b75049d8f1f2b8bd4527b2ccfe53feb743
ᵏᵃᵏᕼᗩᗪᗩ <68880205+hadaIT@users.noreply.github.com>
2021-04-04 12:29:38 +0300

    Create hada.py

---
+++ hada.py
@@ -0,0 +1,19 @@
+print '\n \n \n \n \n \n\n \n \n \n \n \n\n \n \n \n \n \n\n \n \n \n \n \n\n \n \n \n '                        
+        print ' _    _            _      _______ _            _____  _                  _   _  '
+        print '| |  | |          | |    |__   __| |          |  __ \| |                | | | | '
+        print '| |__| | __ _  ___| | __    | |  | |__   ___  | |__) | | __ _ _ __   ___| |_| | '
+        print '|  __  |/ _` |/ __| |/ /    | |  |  _ \ / _ \ |  ___/| |/ _` |  _ \ / _ \ __| | '
+        print '| |  | | (_| | (__|   <     | |  | | | |  __/ | |    | | (_| | | | |  __/ |_|_| '
+        print '|_|  |_|\__,_|\___|_|\_\    |_|  |_| |_|\___| |_|    |_|\__,_|_| |_|\___|\__(_) '
+        print '\n \n \n \n \n \n\n \n \n \n \n \n\n \n \n \n \n \n\n \n \n \n \n \n\n \n \n \n '
+
+
+
+print '1) Test accounts'
+            print '2) Phishing vectors'
+            print '3) OSINT'
+            print '4) Bruteforcing'
+            print '5) Gathering information with credentials'
+            print '6) Miscellaneous'
+            print '7) Configuration'
+            print '8) Take me out of here'
